ec266656404cd315c507616f7452f196744e5287
Joanna <astronautnemo@gmail.com>
2022-12-17 00:17:22 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,4 +1,66 @@
 /* Your Code Here */
+function createEmployeeRecord(array){
+    const employeeRecord = {
+        firstName: array[0],
+        familyName: array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employeeRecord;
+}
+
+function createEmployeeRecords(arrayOfArrays){
+    const arrayOfObjects = [];
+    arrayOfArrays.map(nestedArray => {arrayOfObjects.push(createEmployeeRecord(nestedArray))});
+
+    return arrayOfObjects;
+}
+
+function createTimeInEvent(dateStamp){
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: Number.parseInt(dateStamp.slice(11)),
+        date: dateStamp.slice(0, 10)
+    })
+    return this
+}
+
+function createTimeOutEvent(dateStamp){
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: Number.parseInt(dateStamp.slice(11)),
+        date: dateStamp.slice(0, 10)
+    })
+    return this
+}
+
+function hoursWorkedOnDate(date){
+    for(let i in this.timeInEvents){
+        if(this.timeInEvents[i].date === date){
+            return (this.timeOutEvents[i].hour - this.timeInEvents[i].hour)/100
+        }
+    }
+}
+
+function wagesEarnedOnDate(date){
+    const payOwed = hoursWorkedOnDate.call(this, date) * this.payPerHour;
+    return payOwed;
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+    return srcArray.find(record => {return record.firstName === firstName});
+}
+
+function calculatePayroll(arrayOfEmployees){
+    const reducer = (accumulator, employee) => {
+        let totalWages = allWagesFor.call(employee);
+        return accumulator += totalWages;
+    }
+   return arrayOfEmployees.reduce(reducer, 0)
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
